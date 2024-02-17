adfeee7334ade467262d2b1646aee91d3851c260
ddanilyuk <danis.danilyuk@gmail.com>
2024-02-18 00:36:21 +0200

    Initial Commit

---
+++ ListScopingIssue/ListScopingIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListScopingIssueApp.swift
+//  ListScopingIssue
+//
+//  Created by Denys Danyliuk on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ListScopingIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
